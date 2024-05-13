ac44e8a40e7089967c03891b5f8c5f328ef10563
asfaf1asf <js1jaimdaza@hotmail.com>
2024-05-13 18:05:01 +0700

    hello world

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-function capitalizeWords(str) {
-  return str
-    .split(" ")
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-    .join(" ");
+function longestCommonPrefix(strs) {
+  if (strs.length === 0) return "";
+  let prefix = strs[0];
+  for (let i = 1; i < strs.length; i++) {
+    let j = 0;
+    while (
+      j < prefix.length &&
+      j < strs[i].length &&
+      prefix.charAt(j) === strs[i].charAt(j)
+    ) {
+      j++;
+    }
+    prefix = prefix.substring(0, j);
+  }
+  return prefix;
 }
